a88b3822fd9231731d23824598cc136c4644600c
Zeeshan Butt <zb0255209@gmail.com>
2022-03-06 20:42:28 +0500

    Add Homepage UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {ChakraProvider} from "@chakra-ui/react";
 
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/'} element={<Home />} />
-        <Route path={'/admin'} element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <ChakraProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/admin'} element={<Admin/>}/>
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
+    );
 }
 
 export default App;
